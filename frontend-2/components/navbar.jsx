3220b53665898362da154b9a92a4e4cbd643cb6e
Arpan1108JU <arpankoley345@gmail.com>
2025-04-24 14:14:56 +0530

    fr added 3

---
+++ frontend-2/components/navbar.jsx
@@ -0,0 +1,14 @@
+
+
+  
+import ThemeToggle from "@/components/theme-toggle";
+
+  export default function Navbar() {
+    return (
+      <nav className="flex items-center justify-between p-4 border-b shadow-sm bg-background">
+        <h1 className="text-xl font-semibold">My App</h1>
+        <ThemeToggle />
+      </nav>
+    );
+  }
+  
