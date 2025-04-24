85c4aee96d84064349e187d9ac31850aa0c3b82b
Prasenjit20000 <prasend1008@gmail.com>
2025-04-25 01:37:37 +0530

    1:37

--- frontend-2/app/page.jsx
+++ frontend-2/app/page.jsx
@@ -1,12 +1,15 @@
+"use client"
+
 import React from "react";
-import { CampaignList } from "@/components/campaign-list";
+// import { CampaignList } from "@/components/campaign-list";
+import Campaigns from "./campaign/page";
 
 function Home() {
+  // const {session} = useAuth();
   return (
-    <div className="container mx-auto py-6">
-      <h1 className="text-3xl font-bold mb-6">All Campaigns</h1>
-      <CampaignList />
-    </div>
+    <>
+      <div> Home </div>
+    </>
   );
 }
 
